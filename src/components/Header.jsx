59d8fb660dfc5ded05bbcff40e7624ee3a1cc1d2
Mingo <mingogadeapy@gmail.com>
2025-06-03 00:20:50 -0300

    Delete src directory

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="p-4 bg-gray-900 text-white">
-      <h1 className="text-xl font-bol">NBA Showcase</h1>
-    </header>
-  );
-}
-
-export default Header;
