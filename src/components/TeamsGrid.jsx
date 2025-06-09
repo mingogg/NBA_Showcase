8a05eedb601ffc2c3db8489c536fb439ce72e15b
mingogg <mingogadeapy@gmail.com>
2025-06-08 22:09:38 -0300

    centered team cards

--- src/components/TeamsGrid.jsx
+++ src/components/TeamsGrid.jsx
@@ -185,7 +185,7 @@ function TeamsGrid({ onHoverCard }) {
     },
   ];
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 p-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-6 p-6 w-full justify-items-center">
       {teams.map((team) => (
         <TeamCard
           key={team.name}
